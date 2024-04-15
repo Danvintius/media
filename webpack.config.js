497e95d48528d69c8428017104cdf753b310fbc6
Azerty <danila.94@mail.ru>
2024-04-15 21:44:41 +0300

    temp

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,45 @@
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    
-}
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    })
+  ],
+};
