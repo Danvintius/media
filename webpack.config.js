00d76d474e5e1f997fe3bd28305456f390e9025c
Azerty <danila.94@mail.ru>
2024-08-05 18:42:21 +0300

    comm

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('node:path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // входной файл
+  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
